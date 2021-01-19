3cb1eedf76ccab136ad02195e061856497db3c0c
Deepansha Singh <deepansha.singh5@gmail.com>
2021-01-19 19:47:42 +0530

    Code added for FLOW008

---
+++ C++/FLOW008-Helping_Chef.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for(ll i = a; i <= b; i++)
+#define SQ(a) (a)*(a)
+
+void solve() {
+  // solution
+  int n;
+  cin >> n;
+  if (n < 10) {
+    cout << "Thanks for helping Chef!\n";
+    return;
+  }
+  cout << "-1\n";
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  // test case input
+  ll t;
+  cin >> t;
+  while(t--) {
+    solve();
+  }
+  return 0;
+}
