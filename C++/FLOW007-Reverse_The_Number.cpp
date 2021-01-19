dc6e20c647df8482812fc2d12252ff3006117eac
Deepansha Singh <deepansha.singh5@gmail.com>
2021-01-19 19:47:25 +0530

    Code added for FLOW007

---
+++ C++/FLOW007-Reverse_The_Number.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for(ll i = a; i <= b; i++)
+#define SQ(a) (a)*(a)
+
+void solve() {
+  // solution
+  ll n;
+  cin >> n;
+  int rem;
+  ll rev = 0;
+  while(n) {
+    rem = n % 10;
+    rev = (rev * 10) + rem;
+    n /= 10;
+  }
+  cout << rev << "\n";
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  // test case input
+  ll t;
+  cin >> t;
+  while(t--) {
+    solve();
+  }
+  return 0;
+}
