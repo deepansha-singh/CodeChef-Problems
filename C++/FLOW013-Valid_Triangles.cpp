9cd12d64236192adfbbad80ba69364a4edcad42f
Deepansha Singh <deepansha.singh5@gmail.com>
2021-01-20 21:54:44 +0530

    .cpp code for FLOW013 added

---
+++ C++/FLOW013-Valid_Triangles.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for(ll i = a; i <= b; i++)
+#define SQ(a) (a)*(a)
+
+void solve() {
+  // solution
+  int a, b, c;
+  cin >> a >> b >> c;
+  if (a + b + c == 180) {
+    cout << "YES\n";
+    return;
+  }
+cout << "NO\n";
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  // test case input
+  ll t;
+  cin >> t;
+  while(t--) {
+    solve();
+  }
+  return 0;
+}
